875f2e015b99f6199e0a0e3ceb8c268afe6a5942
9917468 <36746089+9917468@users.noreply.github.com>
2018-06-26 13:41:40 -0700

    Add files via upload

---
+++ scripts/BoidManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidManager : MonoBehaviour {
+
+	public Transform com;
+	private Random r;
+	//public float rangeLimit;
+	public int spawnAmount;
+	public GameObject boid;
+	private GameObject[] boids;
+	public float max;
+	public float min;
+	public Vector3 vel;
+	void Start () {
+		boids = new GameObject[spawnAmount];
+		SpawnBoids ();
+		CalculateCenterOfMass ();
+
+		//rb = GetComponent<Rigidbody> ();
+		//velocity
+		//rb.velocity = GetRandomVector3().normalized;
+		//Location
+		//transform.position = GetRandomVector3();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CalculateCenterOfMass ();
+	}
+
+	public void SpawnBoids(){
+		for(int i=0;i<spawnAmount;i++){
+			boids[i] = Instantiate (boid, GetRandomVector3(), Quaternion.identity);
+		}
+	}
+
+	public Vector3 GetRandomVector3(){
+		vel = new Vector3 (Random.Range(min, max), Random.Range(min, max), Random.Range(min, max));//cahnge to public limit
+		return vel;
+	}
+	public void CalculateCenterOfMass(){
+		float totalX = 0;
+		float totalY = 0; 
+		float totalZ = 0;
+		float avgX = 0;
+		float avgY = 0; 
+		float avgZ = 0;
+
+		for(int i=0;i<spawnAmount;i++){
+			totalX += boids [i].transform.position.x;
+			totalY += boids [i].transform.position.y;
+			totalZ += boids [i].transform.position.z;
+		}
+		avgX = totalX / spawnAmount;
+		avgY = totalY / spawnAmount;
+		avgZ = totalZ / spawnAmount;
+		Vector3 centerOfMass = new Vector3(avgX, avgY, avgZ);
+		Debug.Log (centerOfMass);
+		com.position = centerOfMass;
+	}
+}
---
+++ scripts/Boids.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boids: MonoBehaviour{
+	private Rigidbody rb;
+	public float speed;
+	public Transform com;
+	Color blue = Color.blue;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		rb.velocity = GetRandomVector3 () *0.01f * speed;
+		transform.rotation = Quaternion.LookRotation (rb.velocity);
+		//b.velocity = new Vector3(2,1,3);
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (com);
+		//rotation.x = transform.x + 90;
+		//Physics.Raycast (transform.position, com, 100.0f, 0, QueryTriggerInteraction.Collide);
+		//Ray(transform.position, com.position);
+		Debug.DrawRay(transform.position, com.position, blue, 0.0000000001f, false);
+	}
+	private void SetVel(){
+		Debug.Log (rb.velocity);// Rotation doesn't work because the vector is constatly changing because its a random vector. so i need to sety the vector before trying to rotate the cone
+
+	}
+	public Vector3 GetRandomVector3(){
+		return new Vector3 (Random.Range(-100.0f, 100.0f), Random.Range(-100.0f, 100.0f), Random.Range(-100.0f, 100.0f));
+	}
+
+
+	public void LookAt(Vector3 movement){
+		/*float rotationSpeed = 5.00f;
+		float xRotation = movement.x;
+		float yRotation = movement.y;
+		float zRotation = movement.z;*/
+		//Debug.Log (xRotation + " " + yRotation + " " + zRotation);
+
+		//Quaternion lookingAt = Quaternion.Euler (com.position.x, com.position.y, com.position.z);
+		//transform.rotation = Quaternion.Slerp (transform.rotation, lookingAt, Time.deltaTime * rotationSpeed);
+	}
+	
+}
